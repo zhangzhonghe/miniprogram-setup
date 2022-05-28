b61ad4227345b611ffb402f47a0c942d8c91987d
zhangzhonghe <958414905@qq.com>
2022-05-28 19:08:50 +0800

    chore: export lifecycles

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 export { ComponentWithLifecycle } from './component';
 export { PageWithLifecycle } from './page';
 export { useRefresh } from './useRefresh';
+export { onCreated, onAttached, onDetached, onReady, onMoved, onError } from './componentLifecycle';
+export {
+  onPageLoad,
+  onPageShow,
+  onPageReady,
+  onPageHide,
+  onAddToFavorites,
+  onPageResize,
+  onPageScroll,
+  onPageUnload,
+  onPullDownRefresh,
+  onReachBottom,
+  onSaveExitState,
+  onShareTimeline,
+  onTabItemTap,
+} from './pageLifecycle';
