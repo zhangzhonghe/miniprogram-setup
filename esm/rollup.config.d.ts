0036744680bf59f68568653b59b88a202a07be6a
zhangzhonghe <958414905@qq.com>
2022-06-05 13:06:39 +0800

    chore: 关闭 allowJs 选项

--- esm/rollup.config.d.ts
+++
@@ -1,9 +0,0 @@
-declare namespace _default {
-    const input: string;
-    const output: {
-        format: string;
-        file: string;
-    }[];
-    const plugins: (import("rollup").Plugin | null)[];
-}
-export default _default;
