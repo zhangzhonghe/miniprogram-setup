547f90ede86809aae04b093d869c098ba2f18ad6
zhangzhonghe <958414905@qq.com>
2022-07-03 14:41:18 +0800

    fix: 补充 catch 和 finally

--- src/meta.js
+++ src/meta.js
@@ -1,10 +1,18 @@
 import { useRefresh } from './useRefresh';
 
 const originThen = Promise.prototype.then;
+const originCatch = Promise.prototype.catch;
+const originFinally = Promise.prototype.finally;
 
 if (!originThen.$$isRewritten) {
   Promise.prototype.then = function (onFulfilled, onRejected) {
     return originThen.call(this, useRefresh(onFulfilled), useRefresh(onRejected));
+  };
+  Promise.prototype.catch = function (onRejected) {
+    return originCatch.call(this, useRefresh(onRejected));
+  };
+  Promise.prototype.finally = function (onFinally) {
+    return originFinally.call(this, useRefresh(onFinally));
   };
 
   Promise.prototype.then.$$isRewritten = true;
