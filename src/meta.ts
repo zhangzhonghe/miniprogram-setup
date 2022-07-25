88a1741897d551536d094b1392095b34eecd5c1c
被雨水过滤的空气 <958414905@qq.com>
2022-07-25 12:51:10 +0800

    feat: 使用定时器时支持自动更新视图

--- src/meta.ts
+++ src/meta.ts
@@ -5,6 +5,7 @@ const originCatch = Promise.prototype.catch;
 const originFinally = Promise.prototype.finally;
 
 if (!originThen.$$isRewritten) {
+  // Promise
   Promise.prototype.then = function (onFulfilled, onRejected) {
     return originThen.call(this, useRefresh(onFulfilled), useRefresh(onRejected));
   };
@@ -14,6 +15,14 @@ if (!originThen.$$isRewritten) {
   Promise.prototype.finally = function (onFinally) {
     return originFinally.call(this, useRefresh(onFinally));
   };
+
+  // Timer
+  const originSetTimeout = setTimeout;
+  //@ts-ignore
+  setTimeout = function (this: any, fn: any, ...args: any[]) {
+    return originSetTimeout.call(this, useRefresh(fn)!, ...args);
+  };
+  // TODO: setInterval 好像是基于 setTimeout 实现的？所以只需要替换 setTimeout 即可。
 
   //@ts-ignore
   Promise.prototype.then.$$isRewritten = true;
