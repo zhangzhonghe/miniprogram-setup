5347a9b82e3ead81b4bbf0ca837a54d636790252
被雨水过滤的空气 <958414905@qq.com>
2022-07-25 17:45:46 +0800

    build

---
+++ esm/__tests__/utils/getCom.d.ts
@@ -0,0 +1,1 @@
+export declare const getComponent: (componentName: string) => any;
--- esm/src/useRefresh.d.ts
+++ esm/src/useRefresh.d.ts
@@ -1,5 +1,5 @@
 export declare const refresh: (updateData: (() => void) & {
     isRefreshing?: boolean;
 }) => void;
-export declare const useRefresh: (handler: (...params: any) => any) => ((this: any, ...p: any) => any) | undefined;
+export declare const useRefresh: (handler?: ((...params: any) => any) | null | undefined) => ((this: any, ...args: any) => any) | undefined;
 export declare const nextTick: (handler?: () => void) => Promise<void>;
