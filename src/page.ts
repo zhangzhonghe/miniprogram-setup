dc4586c98631d55df5d5012f8879e3a41839065c
被雨水过滤的空气 <958414905@qq.com>
2022-08-06 17:20:11 +0800

    type: 完善 PageWithSetup 的类型

--- src/page.ts
+++ src/page.ts
@@ -1,14 +1,74 @@
+import { forEachObj } from './shared';
+
 type DataOption = WechatMiniprogram.Component.DataOption;
 type CustomOption = WechatMiniprogram.Page.CustomOption;
 
-interface Setup {
-  (): () => Record<string, any>;
+interface Setup<TData extends DataOption, TCustom extends CustomOption> {
+  (
+    this: never,
+    context: Pick<
+      WechatMiniprogram.Page.Instance<TData, TCustom>,
+      | 'animate'
+      | 'clearAnimation'
+      | 'createIntersectionObserver'
+      | 'createSelectorQuery'
+      | 'getOpenerEventChannel'
+      | 'getPageId'
+      | 'getRelationNodes'
+      | 'getTabBar'
+      | 'groupSetData'
+      | 'hasBehavior'
+      | 'is'
+      | 'options'
+      | 'route'
+      | 'selectAllComponents'
+      | 'selectComponent'
+      | 'selectOwnerComponent'
+      | 'setUpdatePerformanceListener'
+      | 'triggerEvent'
+    >
+  ): () => Record<string, any> | Promise<Record<string, any>>;
 }
 
-type PageOptions = WechatMiniprogram.Page.Options<DataOption, CustomOption> & {
-  setup?: Setup;
+type PageOptions<
+  TData extends DataOption,
+  TCustom extends CustomOption
+> = WechatMiniprogram.Page.Options<DataOption, CustomOption> & {
+  setup?: Setup<TData, TCustom>;
 };
 
-export const PageWithSetup = (options: PageOptions) => {
+export const PageWithSetup = <TData extends DataOption, TCustom extends CustomOption>(
+  options: PageOptions<TData, TCustom>
+) => {
   return Page(options);
+};
+
+const getContext = (instance: any) => {
+  const result: any = {},
+    keys = [
+      'animate',
+      'clearAnimation',
+      'createIntersectionObserver',
+      'createSelectorQuery',
+      'getOpenerEventChannel',
+      'getPageId',
+      'getRelationNodes',
+      'getTabBar',
+      'groupSetData',
+      'hasBehavior',
+      'is',
+      'options',
+      'route',
+      'selectAllComponents',
+      'selectComponent',
+      'selectOwnerComponent',
+      'setUpdatePerformanceListener',
+      'triggerEvent',
+    ];
+
+  for (const key of keys) {
+    result[key] = instance[key];
+  }
+
+  return Object.freeze(result);
 };
